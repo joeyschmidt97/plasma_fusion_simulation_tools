9859a23931be2f2822abc6fb6a9f738f2995833f
Joey NERSC <joseph.schmidt@utexas.edu>
2024-02-29 20:12:57 -0800

    Updated sim watcher to fetch slurm jobs, save to CSV, load from CSV, and smartly fetch previous data.

---
+++ simulation_dashboard/sim_watcher/default_values.py
@@ -0,0 +1,13 @@
+
+username = 'joeschm'
+
+# Pathway to were slurm job data will be saved and referenced
+default_job_csv_path = '/global/homes/j/joeschm/tools/plasma_fusion_simulation_tools/simulation_dashboard/sim_watcher/data'
+default_job_csv_name = 'slurm_jobs_data.csv'
+
+# Pathway to Job IDs that link to output simulations
+default_job_ID_path = ['/global/homes/j/joeschm/tools/GENE']
+
+# Pathway to simulations that are correlated with slurm Job IDs
+default_simulation_path = ['/pscratch/sd/j/joeschm/NSXTU_discharges']
+
